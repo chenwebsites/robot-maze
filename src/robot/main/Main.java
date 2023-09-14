49341f32be59dbde1ac55aa9ce557ca7ae515b24
Chen Geva <65988508+chenwebsites@users.noreply.github.com>
2023-09-14 12:28:34 -0400

    typo fix

--- src/robot/main/Main.java
+++ src/robot/main/Main.java
@@ -3,7 +3,7 @@ package robot.main;
 public class Main {
 
 	// I assume the robot begins his walk from the first square at (6,0)
-	// and after he enter the maze, the entrence becomes a well.
+	// and after he enter the maze, the entrence becomes a wall.
 	// I assume for each maze, there is an exit. 
 	// The robot "walks with his left hand touching" the left wall until he finds
 	// the exit.
