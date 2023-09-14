9f811cd51ae28b15ec466500ca28d32bc517357b
Chen Geva <65988508+chenwebsites@users.noreply.github.com>
2023-09-14 12:26:04 -0400

    fix

--- src/robot/main/Main.java
+++ src/robot/main/Main.java
@@ -2,20 +2,36 @@ package robot.main;
 
 public class Main {
 
+	// I assume the robot begins his walk from the first square at (6,0)
+	// and after he enter the maze, the entrence becomes a well.
+	// I assume for each maze, there is an exit. 
+	// The robot "walks with his left hand touching" the left wall until he finds
+	// the exit.
+
 	public static void main(String[] args) {
 		Robot robot = new Robot();
 
 		while (!robot.hasExited()) {
 
-			for (int i = 0; i < 4; i++) {
-
+			robot.turnLeft();
+			if (robot.isPathClear()) {
+				robot.moveForward();
+				break;
+			} else {
+				for (int i = 0; i < 3; i++) {
+					robot.turnLeft();
+				}
 				if (robot.isPathClear()) {
 					robot.moveForward();
 					break;
-				}
-
-				for (int j = 0; j <= i; j++) {
-					robot.turnLeft();
+				} else {
+					for (int i = 0; i < 2; i++) {
+						robot.turnLeft();
+					}
+					if (robot.isPathClear()) {
+						robot.moveForward();
+						break;
+					}
 				}
 
 			}
